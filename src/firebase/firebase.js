7d3e333feee65550374b19d9aff354dece98b9a8
AlexFirulin <1993firulin@gmail.com>
2023-10-13 14:35:52 +0300

    add hosting

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8dPI6nKeikj25myzf3WFrWICs5H4oq6U",
+  authDomain: "test-for-dzencode.firebaseapp.com",
+  projectId: "test-for-dzencode",
+  storageBucket: "test-for-dzencode.appspot.com",
+  messagingSenderId: "371378324883",
+  appId: "1:371378324883:web:e8226506fea4e69cad5466"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,33 +1,61 @@
 <template>
 <div>
   <ul class="d-flex flex-column">
-    <li v-for="order in ordersArr" :key="order.id" class="order-list d-flex position-relative">
+    <li v-for="order in ordersArr" :key="order.id" class="order-list d-flex position-relative align-items-center">
       <div>{{ order.description }}</div>
       <div>{{ order.title }}</div>
       <div>{{ order.date }}</div>
-      <button class="move-btn"><span>&gt;</span></button>
+      <button class="move-btn" @click="showProd(order)"><span>&gt;</span></button>
       <div>{{ order.products.date }}</div>
-      <div>
-          <ul>
-            <li v-for="product in order.products" :key="product.id">
-              <div>{{ product.name }}</div>
-              <div>{{ product.date }}</div>
+      <transition name="slide">
+      <div v-if="order.showRelated" class="show">
+          <ul class="d-flex px-5 mx-2 align-items-center" >
+            <li v-for="product in order.products" :key="product.id" class="product-list">
+              <div class="d-flex align-items-center p-2 product-card" >
+          <img :src="product.photo" alt="/" />
+          <div class="flex-fill">{{ product.title }} {{ product.type }}</div>
+          <div v-if="product.isNew">
+            <span class="text-primary">свободен</span>
+          </div>
+          <div v-else><span>В ремонте</span></div>
+          <div class="d-flex flex-column">
+            <div>{{ product.guarantee.start }}</div>
+            <div>{{ product.guarantee.end }}</div>
+          </div>
+          <div>
+            <span class="text-primary">{{ product.specification }}</span>
+          </div>
+          <div>{{ product.formattedDate }}</div>
+          <div class="d-flex flex-column">
+            <div v-for="currency in product.price" :key="currency.value">
+              <div>{{ currency.value }}{{ currency.symbol }}</div>
+            </div>
+          </div>
+          <div>Длинное предлинное название прихода{{ product.order }}</div>
+          <div>{{ product.date }}</div>
+        </div>
             </li>
           </ul>
         </div>
+        </transition>
     </li>
-
-
   </ul>
 </div>
 </template>
 
 <script setup>
 import { useOrdersStore } from '../store/orders'
-import { reactive } from "vue"
+import { ref, reactive } from "vue"
 
 const { orders, getOrderProducts } = useOrdersStore()
-let ordersArr = reactive(getOrderProducts)
+const ordersArr = reactive(getOrderProducts.map(order => ({
+  ...order,
+  showRelated: false
+})));
+
+const showProd = (order) => {
+  order.showRelated = !order.showRelated;
+}
 
 
 </script>
@@ -52,5 +80,15 @@ let ordersArr = reactive(getOrderProducts)
 }
 .order-list:hover .move-btn{
   opacity: 1;
+}
+.product-card {
+ gap: 10px;
+ border: 1px solid #646464;
+}
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.5s ease-out;
+}
+.slide-enter, .slide-leave-to {
+  transform: translateX(50%);
 }
 </style>
